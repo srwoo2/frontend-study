2cd17d5fbe4f953b147adafe285c7cf653f39105
srwoo <srwoo@hyundaiht.co.kr>
2025-08-26 16:34:01 +0900

    feat: add flow

--- com.athom.hello/app.ts
+++ com.athom.hello/app.ts
@@ -10,8 +10,17 @@ class MyApp extends Homey.App {
   async onInit() {
     // sdk와 통신
     this.log('MyApp has been initialized');
-  }
 
+    // flow 자바스크립트 추가
+    const card = this.homey.flow.getActionCard('log-something-to-the-console');
+    card.registerRunListener(async (args, state) => {
+      this.log('hello world');
+      this.log(args.something);
+      return true;
+
+      // 보통 연동하거나 조종하거나 하는 로직을 작성한다고 함..
+    });
+  }
 }
 
 module.exports = MyApp;
