052ea0dbc109eb7dfbe48ccc0d97f2845c9a0be7
srwoo <srwoo@hyundaiht.co.kr>
2025-09-24 16:17:19 +0900

    chore: https 환경 구성

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 [![Node.js CI](https://github.com/selim0915/frontend-study/actions/workflows/merge.yml/badge.svg?event=push)](https://github.com/selim0915/frontend-study/actions/workflows/merge.yml)
 
-# react-basic-project
+# Front-study
+
+localhost 환경에서 유효한 https 구성
 
 ```
+// 터미널에서 프로젝트 루트에 인증서 폴더를 만들고 생성
+$ mkdir cert
+openssl req -nodes -new -x509 -keyout cert/key.pem -out cert/cert.pem -days 365
+```
+
+프로젝트 실행
+```
+$ npm install 
 $ node server.js
 ```
+
+테스트 주소
+- webtrc : https://192.168.2.95:3000/src/webrtc/auto-vedio.html
--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const path = require("path");
+const https = require("https");
+const fs = require("fs");
+const WebSocket = require("ws");
 
 const app = express();
 const PORT = 3000;
 
-// webtrc 폴더를 정적 파일 제공
-app.use(express.static(path.join(__dirname, "webtrc")));
+// 정적파일 서빙
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/src", express.static(path.join(__dirname, "src")));
 
-app.listen(PORT, () => {
+// SSL 인증서 경로
+const server = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, "cert/key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "cert/cert.pem"))
+}, app);
+
+// WebSocket (WebRTC 시그널링)
+const wss = new WebSocket.Server({ server });
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  
+  ws.on("message", (message) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+});
+
+// 서버 실행
+server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
